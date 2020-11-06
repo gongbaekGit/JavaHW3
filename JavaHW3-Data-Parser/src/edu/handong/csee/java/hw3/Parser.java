0acbdbfd03d1ec5944f33a1f1b32236e28dfa368
bangseungmin <bangseungmin@172.17.202.153>
2020-11-06 20:53:18 +0900

    add classed
    and Parser, Util.java and its mebers also.

---
+++ JavaHW3-Data-Parser/src/edu/handong/csee/java/hw3/Parser.java
@@ -0,0 +1,19 @@
+package edu.handong.csee.java.hw3;
+
+public class Parser {
+	
+	private String key;
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+	public void printOneResult() {
+		//todo 로직을 추가
+		
+	}
+}
---
+++ JavaHW3-Data-Parser/src/edu/handong/csee/java/hw3/Util.java
@@ -0,0 +1,46 @@
+package edu.handong.csee.java.hw3;
+
+/**
+ * this class contain static methods that dealing string.
+ * @return
+ */
+public class Util {
+	/**
+	 * search that string contain key words. 
+	 * if find, return starting index pointing value.
+	 * @param str . string that we searched
+	 * @param key . key word. that we seaching for.
+	 * @return starting index pointing value
+	 */
+	public static int searchLine(String str, String key) {
+		
+	}
+	
+	/**
+	 * trim string that starting from the next line.
+	 * @return String. contain text that not yet searched.
+	 * @param String. origianl string that  we searched.
+	 */
+	public static String nextLine(String str) {
+		
+	}
+	
+	/**
+	 * treat object json text. 
+	 * this method make new string for object-typed value.
+	 * @return String.
+	 * @param String. original String that searched.
+	 */
+	public static String treatObject(String str) {
+		
+	}
+	/**
+	 * check that string is done or not.
+	 * If string is done, return 1.
+	 * @return int
+	 * @param String. original String that searched.
+	 */
+	public int checkEndString(String str) {
+		
+	}
+}
